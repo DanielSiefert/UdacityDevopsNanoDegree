96222fef2160c455609123daf8b7b9edba429d81
Daniel Siefert <7648401+DanielSiefert@users.noreply.github.com>
2020-01-20 18:53:36 -0500

    Adding shell scripts from running container on k8s

---
+++ run_aws_kubernetes.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+git clone
+
+# Step 1:
+# This is your Docker ID/path
+dockerpath=danielsiefert/devops-capstone
+
+# Step 2
+# Run the Docker Hub container with kubernetes
+kubectl create deployment travel-blog --image=$dockerpath
+
+# Step 3:
+# List kubernetes pods
+kubectl get pods
+
+# Step 4:
+#Give Container 15 seconds to start
+echo "Waiting for container to start..."
+sleep 15
+# Forward the container port to a host
+kubectl port-forward deployment/travel-blog 8000:80
